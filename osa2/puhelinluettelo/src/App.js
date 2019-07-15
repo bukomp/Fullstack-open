721d82eb77fb360239e5c7f44c7ab6ecb2776e3f
bukomp <edvard@shalaev.com>
2019-07-15 13:39:20 +0300

    2.20 puhelinluettelo

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Numbers from './components/Numbers'
 import Filter from './components/Filter'
+import ErrorMessage from './components/ErrorMessage'
 import NewPerson from './components/NewPerson'
 import {post, get, deleteLine, put} from './components/ServerCall'
 
@@ -10,7 +11,11 @@ const App = () => {
   const [ newFilter, setFilter ] = useState('');
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
-
+  const [ newMessage, setMessage] = useState({
+    error: false,
+    show: false,
+    message:''
+  });
   const [ persons, setPersons] = useState([]);
 
   useEffect(()=>{
@@ -19,6 +24,17 @@ const App = () => {
     })
   }, []);
 
+  const hideError = () => {
+    setMessage({
+      error: false,
+      show: false,
+      message:''
+    })
+  }
+
+  const showMessage = (error, message) => {
+    setMessage({error:error, show:true, message:message})
+  }
 
   const handleChange = (e) => {
     switch (e.target.name) {
@@ -37,53 +53,83 @@ const App = () => {
     }
   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if(!(persons.find((person) => {return person.name === newName})||false)){
-
-      post({name:newName, number: newNumber}).then(r => {
+  const addNewPerson = () => {
+    post({name:newName, number: newNumber})
+      .then(r => {
         const tempArr = Array.from(persons);
         tempArr.push(r.data)
         setPersons(tempArr);
         setNewName('');
         setNewNumber('');
+        showMessage(false, `${newName} successfully has been added`)
       })
+      .catch(e => {
+        console.log(e)
+        showMessage(true, `${newName} can't be added with error:s ${e}`)
+      })
+  }
 
-
-    } else if(window.confirm(`${newName} is already added. Replace the old number?`)) {
-      persons.forEach(person => {
-        if (person.name === newName){
-          put(person.id, {name: person.name, number: newNumber})
+  const updatePerson = () => {
+    persons.forEach(person => {
+      if (person.name === newName){
+        put(person.id, {name: person.name, number: newNumber})
           .then(r => {
             get()
               .then(r => {
                 setPersons(r.data)
+                showMessage(false, `${newName} successfully has been updated`)
               })
-
+              .catch(e => {
+                showMessage(true, `${newName} has been updated, but full list couldn't be loaded after with error: ${e}`)
+              })
           })
+          .catch(e => {
+            showMessage(true, `${newName} can't be updated with error: ${e}`)
+          })
+      }
+    })
+  }
 
-        }
+  const deletePerson = (id) => {
+    deleteLine(id).then(r => {
+      get()
+        .then(r => {
+          setPersons(r.data)
+          showMessage(false, `${newName} successfully has been removed`)
+        })
+        .catch(e => {
+        })
+    })
+      .catch(e => {
+        showMessage(true, `${newName} can't be removed with error: ${e}`)
       })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(!(persons.find((person) => {return person.name === newName})||false)){
+      addNewPerson()
+    } else if(window.confirm(`${newName} is already added. Replace the old number?`)) {
+      updatePerson()
     }
   }
 
   const handleDelete = (e) => {
     const id = e.target.id
 
-    if(window.confirm("Are you shure you want to delete this object?")){
-      deleteLine(id).then(r => {
-        get()
-          .then(r => {
-          setPersons(r.data)
-        })
-
-      })
-    }
+    if(window.confirm("Are you shure you want to delete this object?"))deletePerson(id)
   }
 
   return (
     <React.Fragment>
+
+      <ErrorMessage
+        data={newMessage}
+        hideError={hideError}
+      />
+
+
       <h2>Phonebook</h2>
 
       <Filter
