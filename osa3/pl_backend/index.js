43870e72a7fd95374045173c019f4d40da597520
bukomp <edvard@shalaev.com>
2019-07-24 11:51:31 +0300

    backup

--- osa3/pl_backend/index.js
+++ osa3/pl_backend/index.js
@@ -1,32 +1,58 @@
 const express = require('express')
-const url  = require('url');
-
-
+const fs = require('fs')
 const app = express()
 
-const persons = [
-  {
-    "name": "Anton",
-    "number": "42",
-    "id": 6
-  },
-  {
-    "name": "Edvard",
-    "number": "123123",
-    "id": 8
+let persons;
+
+const readFile = () => {
+  fs.readFile('./db', 'utf-8', (err, data)=>{
+  persons = JSON.parse(data).persons;
+  console.log(persons);
+  })
+}
+
+
+app.delete('/api/*', (req,  res) => {
+  const url_parts = req.path.split('/');
+  console.log(url_parts);
+  switch(url_parts[2]){
+    case "persons":
+      let response;
+
+      url_parts[3]?
+
+
+        persons.forEach(person=>{
+
+          if(person.id === +url_parts[3]){
+
+            response = {
+              message: `Contact with id:${+url_parts[3]} has been removed`,
+              data: persons.filter(i => i.id !== +url_parts[3])
+            };
+            fs.writeFile('./db', JSON.stringify({persons:persons.filter(i => i.id !== +url_parts[3])}), err => {
+              if (err) throw err;
+            })
+            res.json(response);
+          }
+        }):res.status(404).json({
+          error: `no data by id:${url_parts[3]} found`
+        });
+      break;
+    default:
+      break;
   }
-]
-
-
+})
 
 app.get('/api/*', (req, res) => {
   const url_parts = req.path.split('/');
   console.log(url_parts);
   switch(url_parts[2]){
     case "persons":
-      let response = `no data by id:${url_parts[3]} found`;
+      let response;
 
-      (url_parts[3])?persons.some(person=>{
+      url_parts[3]?
+        persons.some(person=>{
         if(person.id === +url_parts[3]){
           console.log(person.id);
           response = person;
@@ -34,12 +60,29 @@ app.get('/api/*', (req, res) => {
         }
       }):response = persons;
 
-      res.send(response)
+
+      response?
+        res.send(response)
+        :res.status(404).json({
+          error: `no data by id:${url_parts[3]} found`
+        });
       break;
     default:
       res.send("<h1>no data</h1>")
       break;
   }
+})
+
+app.get('/info', (req, res) => {
+  const info_page = `
+      <div>
+        Phonebook has info for ${persons.length} people
+      </div>
+      <div>
+        ${new Date()}
+      </div>
+    `
+    res.send(info_page);
 })
 
 const PORT = 3001
